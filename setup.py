1495c1480bbdb3ca9c3feecf714729bac1b1eeeb
Andrew Carter <andrew.carter@coheretechnology.com>
2016-04-16 00:36:30 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ import re
 
 
 def get_version():
-      with open(u'klag/__version__.py') as version_file:
+      with open('klag/__version__.py') as version_file:
             return re.search(r"""__version__\s+=\s+(['"])(?P<version>.+?)\1""",
-                 version_file.read()).group(u'version')
+                 version_file.read()).group('version')
 
-setup(name=u'klag',
-      description=u'A Kafka consumer monitoring CLI.',
-      author=u'Andrew Carter',
-      author_email=u'andrew.k.carter@gmail.com',
-      url=u'https://github.com/andrewkcarter/klag',
-      keywords=[u'kafka'],
-      license=u'MIT',
+setup(name='klag',
+      description='A Kafka consumer monitoring CLI.',
+      author='Andrew Carter',
+      author_email='andrew.k.carter@gmail.com',
+      url='https://github.com/andrewkcarter/klag',
+      keywords=['kafka'],
+      license='MIT',
       version=get_version(),
-      packages=[u'klag'],
-      scripts=[u'scripts/klag'],
-      install_requires=[u'kafka-python>=1.0.2'],
+      packages=['klag'],
+      scripts=['scripts/klag'],
+      install_requires=['kafka-python>=1.0.2'],
       )
